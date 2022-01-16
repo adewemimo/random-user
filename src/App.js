5548112bb85e478f424053da7f14cb1d0e2fd7bc
Seun Soetan <seun@strata.ltd>
2022-01-16 18:08:45 -0500

    fetch random users from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://random-data-api.com/api/name/random_name?size=10')
+      .then(response => setData(response.data));
+  }, [count]);
+
+  const handleFetch = () => {
+    setCount(prev => prev + 1);
+    console.log(count);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users</h1>
+      <button onClick={handleFetch}>Refetch</button>
+
+      {data.map(user => (
+        <ul key={user.id}>
+          <li>
+            <p>
+              <span>ID:</span> {user.id}
+            </p>
+            <p>
+              <span>Name:</span> {user.name}
+            </p>
+          </li>
+        </ul>
+      ))}
     </div>
   );
 }
 
 export default App;
+
